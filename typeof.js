7e078766f7bf843dc214b4309ee85bdaf41f9737
Frank Mullen <frankmullen@gmail.com>
2017-04-12 18:04:52 +0100

    added typeof

---
+++ typeof.js
@@ -0,0 +1,11 @@
+console.log(typeof("hello"));
+
+console.log(typeof(2));
+
+console.log(typeof(true));
+
+console.log(typeof({"name":"Frank", "age":41}));
+
+console.log(typeof(function getAge() { return 41;}));
+
+console.log(typeof(name));
