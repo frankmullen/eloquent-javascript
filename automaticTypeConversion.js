4f1251c0029a9e0e9f45773f29273ade1c060b5c
Frank Mullen <frankmullen@gmail.com>
2017-04-12 19:31:32 +0100

    chapter 1

---
+++ automaticTypeConversion.js
@@ -0,0 +1,10 @@
+console.log(8 * null); // 0
+console.log("5" - 1); // 4 - strings can't be substracted
+console.log("5" + 1); // 51 - because strings can be concatenated so JS tries that first
+console.log("five" * 2); // NaN - strings can't be multiplied
+console.log(false == 0); // true
+console.log(true == 0); // false
+console.log(null == undefined); // true
+console.log(null == 0); // false
+console.log(null || "user"); // user
+console.log("Karl" || "user"); // Karl
---
+++ comparisons.js
@@ -0,0 +1,15 @@
+console.log(3 > 2); //true
+console.log(3 < 2); //false
+console.log("Aardvark" < "Zoroaster"); //true
+console.log("aardvark" < "Aardvark"); //false
+console.log("Itchy" != "Scratchy"); //true
+console.log(NaN == NaN); //false - this is the only value in Javascript not equal to itself
+console.log(true && false); //false
+console.log(true && true); //true
+console.log(false || true); //true
+console.log(false || false); //false
+console.log(!false); //true
+console.log(!true); //false
+console.log(1 + 1 == 2 && 10 * 10 > 50); //true
+console.log(true ? 1 : 2); //1 - true selects the middle value
+console.log(false ? 1 : 2); //2 - false selects the right value
